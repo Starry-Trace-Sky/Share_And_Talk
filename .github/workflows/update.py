098733c5544ed5ab7dd2d4bf4aac8ed226b09011
Skyler Sun <3385213313@qq.com>
2023-06-23 21:10:25 +0800

    update action

--- .github/workflows/update.py
+++ .github/workflows/update.py
@@ -9,28 +9,25 @@
 ==================================================
 """
 import re
+import os
 import sys
-from requests import get
+from subprocess import call
+from github import Github
 
 import pysnooper
 
-def getLatestTag():
-    """get repository the latest tag"""
-    url = 'https://api.github.com/repos/Skyler-Sun/Share_And_Talk/tags'
-    req = get(url)
-    if req.status_code == 200:
-        tagName = req.json()[0]['name']
-        return tagName
-    else:
-        return False
 
 with pysnooper.snoop():
     # init vars
     pattern = r"v\d+\.\d+\.\d+"
     # latest tag
-    latestTag = getLatestTag()
-    if not latestTag:
-        sys.exit(1)
+    token = os.environ.get('GITHUB_TOKEN')
+    g = Github(token)
+
+    repo = g.get_repo('Skyler-Sun/Share_And_Talk')
+
+    latestTag = repo.get_tags()[0]
+    tagName = latestTag.name
     # read README.md
     with open('README.md', encoding='utf-8') as f:
         content = f.readlines()
@@ -38,10 +35,20 @@ with pysnooper.snoop():
     Content = ''.join(content)
     # match version position
     result = re.findall(pattern, Content)[0]
-    if result == latestTag:
+    if result == tagName:
         sys.exit()
-    Content = re.sub(pattern, latestTag, Content)
+
+    Content = re.sub(pattern, tagName, Content)
 
     # update REAMDE.md
     with open('README.md', 'w', encoding='utf-8') as f:
         f.write(Content)
+
+    # commit changes
+    call(['git', 'add', '.'])
+    call(['git', 'commit', '-m', 'Auto update README.md version'])
+    call(['git', 'push', 'origin', 'main'])
+
+    # create release
+    tagMessage = latestTag.commit.message
+    repo.create_git_release(tag=tagName, name=tagName, message=tagMessage)
