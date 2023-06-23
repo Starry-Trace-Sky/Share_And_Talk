da7a8e7393a5d1646072a0b504d79e820bbc71df
Skyler Sun <3385213313@qq.com>
2023-06-23 20:29:03 +0800

    Update update.py

--- .github/workflows/update.py
+++ .github/workflows/update.py
@@ -9,24 +9,36 @@
 ==================================================
 """
 import re
-import os
+import sys
+from requests import get
 
 import pysnooper
+
+def getLatestTag():
+    """get repository the latest tag"""
+    url = 'https://api.github.com/repos/Skyler-Sun/Share_And_Talk/tags'
+    req = get(url)
+    if req.status_code == 200:
+        tagName = req.json()[0]['name']
+        return tagName
+    else:
+        return False
 
 with pysnooper.snoop():
     # init vars
     pattern = r"v\d+\.\d+\.\d+"
-    githubRef = os.environ.get("GITHUB_REF")
     # latest tag
-    tag = githubRef.split('/')[-1]
-
+    latestTag = getLatestTag()
+    if not latestTag:
+        sys.exit(1)
+    # read README.md
     with open('README.md', encoding='utf-8') as f:
         content = f.readlines()
-
+    # replace version
     Content = ''.join(content)
     # match version position
     result = re.findall(pattern, Content)[0]
-    Content = re.sub(pattern, tag, Content)
+    Content = re.sub(pattern, latestTag, Content)
 
     # update REAMDE.md
     with open('README.md', 'w', encoding='utf-8') as f:
